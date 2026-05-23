6cc7695485c40cb071c3d1247c95699efbab2819
IvanMembreno <ivanmembreno168@gmail.com>
2026-05-22 18:41:45 -0600

    feat: implementado lista blanca y manejo de tipo de datos

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,25 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	app.use(cookieParser());
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			transform: true,
+		}),
+	);
+
 	await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+	console.error('Error al iniciar la app: ', err);
+	process.exit(1);
+});
