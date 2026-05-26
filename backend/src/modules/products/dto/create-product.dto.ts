8ae0c1b195c1c317dc308a21f528c53afc81c83d
IvanMembreno <ivanmembreno168@gmail.com>
2026-05-25 21:18:35 -0600

    feat: creado el DTO de productos

---
+++ backend/src/modules/products/dto/create-product.dto.ts
@@ -0,0 +1,57 @@
+import {
+	IsString,
+	IsNumber,
+	IsInt,
+	IsUUID,
+	IsPositive,
+	IsNotEmpty,
+	IsOptional,
+	MinLength,
+	Min,
+} from 'class-validator';
+
+export class CreateProductDto {
+	@IsString()
+	@IsNotEmpty()
+	@MinLength(2)
+	name!: string;
+
+	@IsString()
+	@IsOptional()
+	description?: string;
+
+	@IsString()
+	@IsNotEmpty()
+	@MinLength(2)
+	sku!: string;
+
+	@IsString()
+	@IsOptional()
+	barcode?: string;
+
+	@IsString()
+	@IsOptional()
+	imageUrl?: string;
+
+	@IsUUID()
+	@IsNotEmpty()
+	categoryId!: string;
+
+	@IsUUID()
+	@IsNotEmpty()
+	branchId!: string;
+
+	@IsNumber({ maxDecimalPlaces: 2 })
+	@IsPositive()
+	price!: number;
+
+	@IsInt()
+	@IsOptional()
+	@Min(0)
+	stock?: number;
+
+	@IsInt()
+	@IsOptional()
+	@Min(0)
+	minStock?: number;
+}
---
+++ backend/src/modules/products/dto/update-product.dto.ts
@@ -0,0 +1,53 @@
+import {
+	IsString,
+	IsNumber,
+	IsInt,
+	IsUUID,
+	IsPositive,
+	IsOptional,
+	MinLength,
+	Min,
+} from 'class-validator';
+
+export class UpdateProductDto {
+	@IsString()
+	@MinLength(2)
+	@IsOptional()
+	name?: string;
+
+	@IsString()
+	@IsOptional()
+	description?: string;
+
+	@IsString()
+	@MinLength(2)
+	@IsOptional()
+	sku?: string;
+
+	@IsString()
+	@IsOptional()
+	barcode?: string;
+
+	@IsString()
+	@IsOptional()
+	imageUrl?: string;
+
+	@IsUUID()
+	@IsOptional()
+	categoryId?: string;
+
+	@IsNumber({ maxDecimalPlaces: 2 })
+	@IsPositive()
+	@IsOptional()
+	price?: number;
+
+	@IsInt()
+	@Min(0)
+	@IsOptional()
+	stock?: number;
+
+	@IsInt()
+	@Min(0)
+	@IsOptional()
+	minStock?: number;
+}
