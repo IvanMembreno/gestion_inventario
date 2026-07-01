0354712d81e10afe966eaf574dcf779f3560b7f5
IvanMembreno <ivanmembreno168@gmail.com>
2026-06-30 23:34:26 -0600

    feat: módulo products completo, schema final con Shelf, ShoppingList y mejoras

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './config/prisma.service.js';
 import { BranchesModule } from './modules/branches/branches.module.js';
 import { CategoriesModule } from './modules/categories/categories.module.js';
+import { ProductsModule } from './modules/products/products.module.js';
 
 @Module({
 	imports: [
@@ -11,6 +12,7 @@ import { CategoriesModule } from './modules/categories/categories.module.js';
 		}),
 		BranchesModule,
 		CategoriesModule,
+		ProductsModule,
 	],
 	controllers: [],
 	providers: [PrismaService],
